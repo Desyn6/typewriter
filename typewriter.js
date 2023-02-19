96b0cab9b794f41f73ddc80218715d12eb194836
Denny <Dennyng1989@hotmail.com>
2023-02-19 15:13:00 -0800

    commit exercise 1 to repo

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "Hello from the fourth week at Lighthouse labs!";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
+
+setTimeout(() => console.log('\n'), delay);
